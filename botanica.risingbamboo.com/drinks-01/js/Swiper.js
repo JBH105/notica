351922505ad69c24d086f2a8dae70c79c81a8ccb
jbh105 <jaydipbhayani105@gmail.com>
2025-05-05 18:19:20 +0530

    design fixing side padding

--- botanica.risingbamboo.com/drinks-01/js/Swiper.js
+++ botanica.risingbamboo.com/drinks-01/js/Swiper.js
@@ -15,6 +15,10 @@ const shopMapsSwiper = new Swiper('.shop-maps-swiper', {
         prevEl: '.shop-maps-prev',
     },
     breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
         768: {
             slidesPerView: 2,
             spaceBetween: 30,
@@ -25,3 +29,36 @@ const shopMapsSwiper = new Swiper('.shop-maps-swiper', {
         },
     },
 });
+
+
+// const swiper = new Swiper('.services-slider', {
+// 	slidesPerView: 3,
+// 	spaceBetween: 20,
+// 	loop: true,
+// 	autoplay: {
+// 		delay: 3000,
+// 		disableOnInteraction: false,
+// 	},
+// 	navigation: {
+// 		nextEl: '.swiper-button-next',
+// 		prevEl: '.swiper-button-prev',
+// 	},
+// 	scrollbar: {
+// 		el: '.swiper-scrollbar',
+// 		draggable: true,
+// 	},
+// 	breakpoints: {
+// 		0: {
+// 			slidesPerView: 1,
+// 			spaceBetween: 10,
+// 		},
+// 		768: {
+// 			slidesPerView: 2,
+// 			spaceBetween: 15,
+// 		},
+// 		1200: {
+// 			slidesPerView: 3,
+// 			spaceBetween: 20,
+// 		},
+// 	},
+// });
