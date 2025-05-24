9e4d5bfa591677a2134a8cac477609fe238ca4ec
jbh105 <jaydipbhayani105@gmail.com>
2025-05-24 12:59:19 +0530

    added map and reviews

--- botanica.risingbamboo.com/drinks-01/assets/js/FranchiseSwiper.js
+++ botanica.risingbamboo.com/drinks-01/assets/js/FranchiseSwiper.js
@@ -1,30 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-    new Swiper('.swiper-container', {
+  new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerGroup: 1,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1024: {
         slidesPerView: 3,
-        spaceBetween: 20,
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        slidesPerGroup: 1, 
-        breakpoints:  {
-            0: {
-                slidesPerView: 1,
-                spaceBetween: 10,
-            },
-            768: {
-                slidesPerView: 2,
-                spaceBetween: 30,
-            },
-            1024: {
-                slidesPerView: 3,
-                spaceBetween: 30,
-            },
-        }
-    });
+        spaceBetween: 30,
+      },
+    }
+  });
 });
